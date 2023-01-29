fd9a8ca7038e6bf695a5d33275d9330ad09cb41a
babucarr32 <baboucarry@gmail.com>
2023-01-29 15:18:07 +0000

    if user not found redirect to signup

--- Login/views.py
+++ Login/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        userLogin = auth.authenticate(username = request.POST['username'], password = request.POST['password']) 
+        if userLogin is not None:
+            auth.login(request, userLogin)
+            return redirect('/')
+        else:
+            return render(request, 'signup.html')
+    else:
+        return render(request, 'login.html')
+            
